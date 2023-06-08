ee9d65391d18aee2ce863d0a05a136449eb7909e
dmitriy <dima22022005@gmail.com>
2023-06-08 23:29:11 +0200

    Add a function for handling image uploading. Whenever the user uploads an image, the label changes its name into the file name of an image. Also a camera svg image changes into user's image to preview their file

--- components/form.js
+++ components/form.js
@@ -1,6 +1,19 @@
 import styles from '../styles/Form.module.scss'
+import React, { useState } from 'react'
 
 export default function Form() {
+
+    const [imageName, setImageName] = useState("Kies bestand")
+    const [imagePreview, setImagePreview] = useState("images/camera.svg")
+
+    function handleImageUpload (event) {
+        if (event !== undefined) {
+            const file = event.target.files[0];
+            setImageName(file.name)
+            setImagePreview(URL.createObjectURL(file))
+        }
+    }
+
     return (
         <div className={styles.form}>
         <h1>Plaats een blog bericht</h1>
@@ -18,8 +31,12 @@ export default function Form() {
         <div className={styles.formElement}>
           <h3>Header afbeelding</h3>
           <div className={styles.imgUpload}>
+            {imagePreview &&
+              <img src={imagePreview}/>
+            }
             <label maxLength={10} htmlFor="file-input">
-              <input id="file-input" type="file"accept="image/*"/>
+              {imageName}
+              <input id="file-input" type="file" onChange={handleImageUpload} accept="image/*"/>
             </label>
           </div>
         </div>
