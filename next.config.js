355682df2a17cc1259778e107b7ba123937f8040
dmitriy <dima22022005@gmail.com>
2023-06-08 12:02:03 +0200

    Setup project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+const path = require('path');
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+  <main>
+    <Component {...pageProps} />
+  </main>
+)}
