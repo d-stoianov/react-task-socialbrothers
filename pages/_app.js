fbacf7536efcf2476966f707d2b019d78db5a115
dmitriy <dima22022005@gmail.com>
2023-06-08 23:17:29 +0200

    Add global font Inter installed from next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '@/styles/globals.scss'
+import { Inter } from '@next/font/google'
+
+// global font
+const inter = Inter({
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }) {
   return (
-  <main>
+  <main className={inter.className}>
     <Component {...pageProps} />
   </main>
 )}
