550ba03bad22747cd835667cfab18c35e065c2d4
dmitriy <dima22022005@gmail.com>
2023-06-14 03:12:54 +0200

    Add loading and success toasts while clicking on pages. Disable clicking on pages while loading

--- pages/blog.js
+++ pages/blog.js
@@ -5,6 +5,8 @@ import Post from "@/components/post"
 import Footer from "@/components/footer"
 import { useState } from "react"
 import ReactPaginate from 'react-paginate'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { ApiService } from "@/service/api-service"
 
 const service = new ApiService()
@@ -23,10 +25,24 @@ export async function getServerSideProps() {
 
 export default function Blog({ initialPosts, totalPostsAmount }) {
 	const [currentPosts, setCurrentPosts] = useState(initialPosts)
+	const [loading, setLoading] = useState(false)
 
 	async function handlePageClick(event) {
+		const loaderToast = toast.info('Loading page...')
+		setLoading(true)
+
 		const pageNumber = event.selected + 1
-		setCurrentPosts((await service.getPosts(pageNumber, pageSize)).posts)
+		const response = await service.getPosts(pageNumber, pageSize)
+
+		toast.dismiss(loaderToast)
+		setLoading(false)
+
+		if (!response.success) {
+			toast.error(response.errorMessage)
+		} else {
+			toast.success("Successful !")
+			setCurrentPosts(response.posts)
+		}
 	}
 
 	return (
@@ -37,6 +53,18 @@ export default function Blog({ initialPosts, totalPostsAmount }) {
 			</Head>
 			<Header title="Blog" />
 			<div className={styles.container}>
+				<ToastContainer
+					position="top-right"
+					autoClose={2000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="dark"
+				/>
 				<div className={styles.containerContent}>
 					{currentPosts.map((post, index) => {
 						return <Post key={index} title={post.title} date={post.date} category={post.category}
@@ -52,7 +80,7 @@ export default function Blog({ initialPosts, totalPostsAmount }) {
 					pageCount={totalPostsAmount / pageSize}
 					renderOnZeroPageCount={null}
 					onPageChange={handlePageClick}
-					containerClassName={styles.pagination}
+					containerClassName={!loading ? styles.pagination : styles.disabledPagination}
 					pageClassName={styles.pageItem}
 					nextLinkClassName={styles.nextButton}
 					activeClassName={styles.selectedPage}
